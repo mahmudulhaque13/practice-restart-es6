b90761afa4e1f980870ea89697af0a03d648eb16
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-15 22:26:15 +0600

    filter

--- 1.js
+++ 1.js
@@ -1,8 +1,16 @@
-const users = [
-  { name: "Rahim", email: "rahim@gmail.com" },
-  { name: "Karim", email: "karim@gmail.com" },
-];
+// const users = [
+//   { name: "Rahim", email: "rahim@gmail.com" },
+//   { name: "Karim", email: "karim@gmail.com" },
+// ];
 
-const emails = users.map(({ email }) => email);
+// const emails = users.map(({ email }) => email);
 
-console.log(emails);
+// console.log(emails);
+
+// filter
+
+const numbers = [10, 60, 45, 80, 30, 90];
+
+const greaterThan50 = numbers.filter((n) => n > 50);
+
+console.log(greaterThan50);
