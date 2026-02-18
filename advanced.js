7b6cd11f30054b271fc86f43465a517ac2e23c41
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-18 23:12:17 +0600

    deep clone

--- advanced.js
+++ advanced.js
@@ -1,13 +1,31 @@
 // Polyfill: Array.prototype.map
 // নিজে থেকে map() function বানাও (polyfill)।
-Array.prototype.myMap = function (callback) {
-  const result = [];
-  for (let i = 0; i < this.length; i++) {
-    result.push(callback(this[i], i, this));
+// Array.prototype.myMap = function (callback) {
+//   const result = [];
+//   for (let i = 0; i < this.length; i++) {
+//     result.push(callback(this[i], i, this));
+//   }
+//   return result;
+// };
+
+// // test
+// const nums = [1, 2, 3];
+// console.log(nums.myMap((n) => n * 2)); // [2,4,6]
+
+// Deep Clone Object (Without JSON)
+// nested object deep copy করো।
+const obj = { a: 1, b: { c: 2 } };
+const deepClone = (obj) => {
+  if (obj === null || typeof obj !== "object") return obj;
+
+  const copy = Array.isArray(obj) ? [] : {};
+  for (let key in obj) {
+    copy[key] = deepClone(obj[key]);
   }
-  return result;
+  return copy;
 };
 
-// test
-const nums = [1, 2, 3];
-console.log(nums.myMap((n) => n * 2)); // [2,4,6]
+const clone = deepClone(obj);
+clone.b.c = 99;
+
+console.log(obj.b.c); // 2
