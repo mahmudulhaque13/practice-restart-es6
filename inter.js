69593d25e846049706dc86094139fa029f3846ce
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-20 17:41:16 +0600

    Count Vowels in String

--- inter.js
+++ inter.js
@@ -1,9 +1,17 @@
 // একটি sentence এর প্রতিটি word-এর প্রথম letter uppercase করো।
-const text = "javascript is awesome";
-const capitalized = text
-  .split(" ")
-  .map((word) => word[0].toUpperCase() + word.slice(1))
-  .join(" ");
+// const text = "javascript is awesome";
+// const capitalized = text
+//   .split(" ")
+//   .map((word) => word[0].toUpperCase() + word.slice(1))
+//   .join(" ");
 
-console.log(capitalized);
-// JavaScript Is Awesome
+// console.log(capitalized);
+// // JavaScript Is Awesome
+
+// Count Vowels in String
+const str = "programming";
+const vowels = ["a", "e", "i", "o", "u"];
+
+const count = [...str].filter((ch) => vowels.includes(ch)).length;
+
+console.log(count); // 3
