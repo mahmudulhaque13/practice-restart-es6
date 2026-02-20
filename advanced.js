6ce898089fc700eb2328e779ce1f3e6f694aafd8
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-20 17:24:41 +0600

    Immutable Deep Update

--- advanced.js
+++ advanced.js
@@ -187,21 +187,40 @@
 
 // Promise Pool (Concurrency Control)
 // একসাথে max N promise চলবে।
-const promisePool = async (tasks, limit) => {
-  const results = [];
-  const executing = [];
+// const promisePool = async (tasks, limit) => {
+//   const results = [];
+//   const executing = [];
 
-  for (const task of tasks) {
-    const p = Promise.resolve().then(task);
-    results.push(p);
+//   for (const task of tasks) {
+//     const p = Promise.resolve().then(task);
+//     results.push(p);
 
-    if (limit <= tasks.length) {
-      const e = p.then(() => executing.splice(executing.indexOf(e), 1));
-      executing.push(e);
-      if (executing.length >= limit) {
-        await Promise.race(executing);
-      }
-    }
-  }
-  return Promise.all(results);
+//     if (limit <= tasks.length) {
+//       const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+//       executing.push(e);
+//       if (executing.length >= limit) {
+//         await Promise.race(executing);
+//       }
+//     }
+//   }
+//   return Promise.all(results);
+// };
+
+// Immutable Deep Update
+// nested object update করো without mutation।
+const state = {
+  user: {
+    profile: { name: "Rahim", age: 25 },
+  },
+};
+
+const newState = {
+  ...state,
+  user: {
+    ...state.user,
+    profile: {
+      ...state.user.profile,
+      age: 26,
+    },
+  },
 };
