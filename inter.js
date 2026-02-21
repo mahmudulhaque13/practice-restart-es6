a2b816c57fa5b049972e1a7e73d0d4a0da6b0c98
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-21 17:22:17 +0600

    separate word

--- inter.js
+++ inter.js
@@ -23,8 +23,15 @@
 // console.log(length); // 11
 
 // string এর মধ্যে নির্দিষ্ট word আছে কিনা চেক করো
-const sentence = "I love JavaScript programming";
-const word = "JavaScript";
+// const sentence = "I love JavaScript programming";
+// const word = "JavaScript";
 
-const result = sentence.includes(word);
-console.log(result); // true
+// const result = sentence.includes(word);
+// console.log(result); // true
+
+// একটি sentence থেকে সব word আলাদা করো
+const sentence = "I love JavaScript programming";
+
+const words = sentence.split(" ");
+console.log(words);
+// ["I", "love", "JavaScript", "programming"]
