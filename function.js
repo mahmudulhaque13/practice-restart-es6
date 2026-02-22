31fdafea2bff455ed8505b388369fdeaf3f7c561
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-22 23:04:57 +0600

    even/odd

--- function.js
+++ function.js
@@ -1,7 +1,14 @@
-function sum(a, b) {
-  return a + b;
-}
+// দুটি সংখ্যার যোগফল বের করার function লেখো
+// function sum(a, b) {
+//   return a + b;
+// }
 
-// ব্যবহার
-const result = sum(10, 20);
-console.log(result); // 30
+// // ব্যবহার
+// const result = sum(10, 20);
+// console.log(result); // 30
+
+// even/odd চেক করার function লেখো
+const checkEvenOdd = (num) => (num % 2 === 0 ? "Even" : "Odd");
+
+console.log(checkEvenOdd(4)); // Even
+console.log(checkEvenOdd(9)); // Odd
