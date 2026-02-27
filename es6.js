f48e2684a28c28dae18d62c3da9f0697bc9d7483
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-27 22:56:53 +0600

    Conditional State Update

--- es6.js
+++ es6.js
@@ -1,18 +1,33 @@
 // Multiple setState Batching (React behavior simulation)
-let count = 0;
+// let count = 0;
 
-const setCount = (updater) => {
-  count = typeof updater === "function" ? updater(count) : updater;
+// const setCount = (updater) => {
+//   count = typeof updater === "function" ? updater(count) : updater;
+// };
+
+// setCount(count + 1);
+// setCount(count + 1);
+
+// console.log("Wrong way:", count); // 1 ❌
+
+// // correct
+// count = 0;
+// setCount((prev) => prev + 1);
+// setCount((prev) => prev + 1);
+
+// console.log("Correct way:", count); // 2 ✅
+
+// Conditional State Update (Avoid Unnecessary Render)
+let theme = "light";
+
+const setTheme = (newTheme) => {
+  if (theme !== newTheme) {
+    theme = newTheme;
+    console.log("Theme changed:", theme);
+  } else {
+    console.log("No re-render");
+  }
 };
 
-setCount(count + 1);
-setCount(count + 1);
-
-console.log("Wrong way:", count); // 1 ❌
-
-// correct
-count = 0;
-setCount((prev) => prev + 1);
-setCount((prev) => prev + 1);
-
-console.log("Correct way:", count); // 2 ✅
+setTheme("light");
+setTheme("dark");
