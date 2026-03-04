b9d6ebcd9b471b70ba796ed0dd048a4272dfdf9b
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-04 23:42:12 +0600

    same id duplicate remove

--- more.js
+++ more.js
@@ -1,12 +1,23 @@
 // Nested object copy করো।
-const obj = {
-  name: "Mahmud",
-  address: { city: "Dhaka" },
-};
+// const obj = {
+//   name: "Mahmud",
+//   address: { city: "Dhaka" },
+// };
 
-const clone = structuredClone(obj);
+// const clone = structuredClone(obj);
 
-clone.address.city = "Chittagong";
+// clone.address.city = "Chittagong";
 
-console.log("Original:", obj);
-console.log("Clone:", clone);
+// console.log("Original:", obj);
+// console.log("Clone:", clone);
+
+// Same id থাকলে duplicate remove করো।
+const products = [
+  { id: 1, name: "Pen" },
+  { id: 2, name: "Book" },
+  { id: 1, name: "Pen" },
+];
+
+const unique = [...new Map(products.map((p) => [p.id, p])).values()];
+
+console.log(unique);
